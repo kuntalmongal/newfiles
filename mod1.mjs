f146cf5a272e0b9b4931a6c679d762ef26a6b5eb
kuntalmongal <108890220+kuntalmongal@users.noreply.github.com>
2023-04-29 00:29:00 +0530

    Updates

---
+++ mod1.mjs
@@ -0,0 +1,10 @@
+const a="Kuntal";
+const b="Arup";
+const c="Patra";
+const d="Ankan";
+
+export default d;
+
+export {a};
+export {b};
+export {c};
---
+++ mod2.mjs
@@ -0,0 +1,5 @@
+import dza , {a,b,c} from './mod1.mjs'
+console.log(dza);
+console.log(b);
+console.log(a);
+console.log(c);
